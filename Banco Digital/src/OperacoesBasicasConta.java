24885c088b778019717234c03ad53d71fd501656
guigilmoura <gui.gil.moura@gmail.com>
2022-05-30 20:12:20 -0300

    deletando duplicatas

--- Banco Digital/src/OperacoesBasicasConta.java
+++
@@ -1,9 +0,0 @@
-public interface OperacoesBasicasConta {
-    void depositar(double valor);
-    void sacar(double valor);
-    void transferir(double valor, Conta destino);
-    void imprimirHistorico();
-    void imprimirSaldo();
-    void imprimirRendimentoEstimado();
-    double calcularRendimento();
-}
